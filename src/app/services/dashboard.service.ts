9d5bfeb16d13aa2a7bdaa4edc13579de212d9921
Alican Çevik <alican.cevik@hotmail.com>
2020-10-29 12:36:40 +0300

    DashboardServiceErrorHandleAdded

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Dashboard } from '../models/dashboard';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +16,23 @@ export class DashboardService {
   constructor(private http: HttpClient) { }
 
   getDashboardDetails(){
-    return this.http.get<Dashboard>(this.path);
+    return this.http.get<Dashboard>(this.path).pipe(
+      retry(1),
+      catchError(this.handleError)
+    );
+  }
+
+  handleError(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // client-side error
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
   }
 
 }
